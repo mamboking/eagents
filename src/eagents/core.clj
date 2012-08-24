469f851f0a87110887e6ac93be3c0b3866b55ee1
mamboking <khostelley@yahoo.com>
2012-08-24 16:14:02 -0400

    Added query-and-alter

--- src/eagents/core.clj
+++ src/eagents/core.clj
@@ -1,1 +1,11 @@
 (ns eagents.core)
+
+(defn query-and-alter [a query-fn alter-fn]
+  (let [p (promise)
+        f (fn [x]
+            (deliver p (query-fn x))
+            (alter-fn x))]
+    (send a f)
+    @p))
+
+    
